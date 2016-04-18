188fcef4a078dc84804bfa9efa6a273d74b5fbbc
p1ch_jp <p1ch.jp@gmail.com>
2016-04-18 20:10:52 +0900

    Set up a Electron development environment with ES6 and Vue.js

---
+++ app/browser/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import app from 'app';
+import BrowserWindow from 'browser-window';
+import path from 'path';
+
+let mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + path.resolve(__dirname, '../renderer/index.html'));
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
